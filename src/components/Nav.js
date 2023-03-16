949ef2e5a3345b120de1b2c61cb05bf5a204c1bf
TECHIE SAM <blessingabolade15@gmail.com>
2023-03-16 12:45:24 +0100

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,24 @@
+import React , { useState } from 'react'
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import PersonIcon from '@mui/icons-material/Person';
+import CollectionsBookmarkRoundedIcon from '@mui/icons-material/CollectionsBookmarkRounded';
+import FolderCopyOutlinedIcon from '@mui/icons-material/FolderCopyOutlined';
+import SpeakerNotesIcon from '@mui/icons-material/SpeakerNotes';
+
+const Nav = () => {
+    const [activeNav, setActiveNav] = useState("#")
+    return (
+        <>
+            <nav>
+                <a href="#" aria-labelledby onClick={() => setActiveNav("#")} className={activeNav === '#' ? 'active' : " "}><HomeRoundedIcon /></a>
+                <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === "#about" ? "active" : " "}><PersonIcon/></a>
+                <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === "#experience" ? "active" : " "}><CollectionsBookmarkRoundedIcon /></a>
+                <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === "#services" ? "active" : " "}><FolderCopyOutlinedIcon /></a>
+                <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === "#contact" ? "active" : " "}><SpeakerNotesIcon /></a>
+            
+            </nav>
+        </>
+    )
+}
+
+export default Nav
