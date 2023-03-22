8e55c0e7031cd6f81b0402d5e007d3692e8523c7
TECHIE SAM <blessingabolade15@gmail.com>
2023-03-22 17:04:46 +0100

    added circular-progressbar to skills

---
+++ src/components/skills/EachSkill.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+const EachSkill = styled.div`
+    .skill{
+        width:65px
+    }
+`
+export default EachSkill
--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import StyledCard from '../about/StyledCard'
 import VerifiedIcon from '@mui/icons-material/Verified';
-const skills = ['HTML', 'CSS', 'JAVASCRIPT', ]
+import { CircularProgressbar, CircularProgressbarWithChildren, buildStyles } from "react-circular-progressbar";
+import 'react-circular-progressbar/dist/styles.css';
+import EachSkill from './EachSkill';
+const skills = [
+    { skill: "HTML", percentage: "90" },
+    { skill: "CSS", percentage: "75" },
+    { skill: "Javascript", percentage: "80" }
+]
+
 const Skills = () => {
     return (
+
         <section className='mt-5'>
             <div className='text-center mb-3'>
                 <h6>Get to know</h6>
@@ -14,42 +23,20 @@ const Skills = () => {
                     <span className='col' >
                         <h3>Language</h3>
                         <div className="">
-                            <small className="" data-aos="fade-right">
-                                <VerifiedIcon className="" />
-                                <div><h4>HTML</h4>
-                                </div>
-                            </small>
-                            <small className="" data-aos="fade-left">
-                                <VerifiedIcon className="" />
-                                <small>  <h4>CSS</h4>
-                                </small>
-                            </small>
-                            <small className="" data-aos="fade-right">
-                                <VerifiedIcon className="" />
-                                <div>
-                                    <h4>JavaScript</h4>
+                            <div className='row' >
+                                {
+                                    skills.map(({ skill, percentage }) =>
+                                        <div style={{ width: "10px" }} className="col mt-3 border">
+                                            <div className='border' style={{ width: "70px" }}>
 
-                                </div>
-                            </small>
-                            <small className="" data-aos="fade-left">
-                                <VerifiedIcon className="" />
-                                <div><h4>TypeScript</h4>
-                                </div>
-                            </small>
-                            <small className="" data-aos="fade-right">
-                                <VerifiedIcon className="" />
-                                <div>
-                                    <h4>React JS</h4>
+                                                <CircularProgressbar className='border m-auto col row' style={{ width: "70px" }} value={percentage} text={`${percentage}%`} />
+                                            </div>
+                                            <h4 className='mt-3 fs-6 '> {skill}</h4>
+                                        </div>
+                                    )
+                                }
+                            </div>
 
-                                </div>
-                            </small>
-                            <small className="experience__details" data-aos="fade-right">
-                                <VerifiedIcon className="experience__details-icon" />
-                                <div>
-                                    <h4>Next JS</h4>
-
-                                </div>
-                            </small>
 
                         </div>
                     </span>
@@ -59,8 +46,23 @@ const Skills = () => {
                     </span>
                 </div>
             </StyledCard >
+
         </section >
     )
 }
 
+// function Example(props) {
+//     return (
+//         <div style={{ marginBottom: 80 }}>
+//             <hr style={{ border: "2px solid #ddd" }} />
+//             <div style={{ marginTop: 30, display: "flex" }}>
+//                 <div style={{ width: "30%", paddingRight: 30 }}>{props.children}</div>
+//                 <div style={{ width: "70%" }}>
+//                     <h3 className="h5">{props.label}</h3>
+//                     <p>{props.description}</p>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
 export default Skills
