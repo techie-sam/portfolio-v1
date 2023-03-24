ed67921ebbb2801abd84a8f1ace3603c2a18c247
TECHIE SAM <blessingabolade15@gmail.com>
2023-03-24 12:29:57 +0100

    contact section done

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,31 +1,55 @@
 import React from 'react'
 import StyledCard from '../about/StyledCard'
+import TextField from '@mui/material/TextField';
 
 const Contact = () => {
     const CONTACT_DETAILS = [
         { platform: "Email", address: "blessingabolade15@gmail.com", link: "https//b;essingabolade15@gmail.com", icon: '' },
-        { platform: "LinkedIn", address: "Send a message", link: "https//linkedin.com/in/techie-sam", icon: '' },
-        { platform: "WhatsApp", address: "Send a message", icon: '' },
+        { platform: "WhatsApp", address: "Send a message",},
         { platform: "Email", address: "blessingabolade15@gmail.com", icon: '' },
+    ]
+    const TEXT_FIELD_PROPS = [
+        { id: "fullname", label: "Full Name", defaultValue: ""},
+        { id: "email", label: "Email", defaultValue: ""},
+        { id: "message", label: "Message", defaultValue: "", rows: "4", multiline: true },
     ]
     return (
         <section id="contact">
             <div className='text-center mt-5'>
                 <h6>Get in Touch</h6>
-                <h5>Contact</h5>
+                <h5>Contact Me</h5>
             </div>
-            <StyledCard className='row gap-3 col-lg-4 p-lg-5'>
-                {
-                    CONTACT_DETAILS.map(({ platform, address, link, icon }, i) =>
-                        <span key={platform}>
-                            <a href={link} className="text-decoration-none">
-                                <h6>{platform}</h6>
-                                <small className='text-light d-block mb-3'>{address}</small>
-                                <small>Send Message</small>
-                            </a>
-                        </span>
-                    )
-                }
+            <StyledCard className='row gap-5'>
+                <div className='row gap-3 col-lg-4 col-12 m-auto'>
+                    {
+                        CONTACT_DETAILS.map(({ platform, address, link, icon }, i) =>
+                            <span key={platform}>
+                                <a href={link} className="text-decoration-none">
+                                    <h6>{platform}</h6>
+                                    <small className='text-light d-block mb-3'>{address}</small>
+                                    <small>Send Message</small>
+                                </a>
+                            </span>
+                        )
+                    }
+                </div>
+                <div className='col col-lg-6 rounded rounded-5'>
+                    {
+                        TEXT_FIELD_PROPS.map(({ id, label, defaultValue, rows, multiline }) =>
+                            <TextField
+                                className='d-block mt-4'
+                                fullWidth
+                                id={id}
+                                label={label}
+                                defaultValue={defaultValue}
+                                focused
+                                rows={rows}
+                                multiline
+                            />
+                        )
+                    }
+                    <button className='btn btnPrimary mt-3'>SEND MESSAGE</button>
+                </div>
             </StyledCard>
         </section>
     )
