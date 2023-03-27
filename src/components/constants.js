2211fa31f1f2fb26180ef47b81361b5a8ba2dbf7
TECHIE SAM <blessingabolade15@gmail.com>
2023-03-27 17:21:46 +0100

    update contact links

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,7 +1,7 @@
-const linkedinAddress = 'https://linkedin.com/in/techie-sam'
-const twitterAddress = 'https://linkedin.com/techie_sam'
+const linkedinAddress = 'https://www.linkedin.com/in/techie-sam'
+const twitterAddress = 'https://twitter.com/techie_sam'
 const githubAddress = 'https://github.com/techie-sam'
-const whatsappLink = 'https://'
-const emailAddress = 'https://emailAddreess'
+const whatsappLink = 'https://wa.me/+2349061918327'
+const emailAddress = 'mailto:blessingabolade15@gmail.com'
 
 export { linkedinAddress, twitterAddress, githubAddress, whatsappLink, emailAddress }
