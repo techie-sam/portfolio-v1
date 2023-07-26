56ddaefe93ab19c98f29d97df1521a9b75410390
TECHIE SAM <blessingabolade15@gmail.com>
2023-07-26 20:41:22 +0100

    changed to Im

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,32 +1,33 @@
-import StyledHeader from './StyledHeader';
-import CTA from './CTA';
-import { motion } from 'framer-motion';
-import { childVariant, containerVariant } from './HeaderAnimationStyles';
+import StyledHeader from "./StyledHeader";
+import CTA from "./CTA";
+import { motion } from "framer-motion";
+import { childVariant, containerVariant } from "./HeaderAnimationStyles";
 
 const items = [
-  <h5 style={{fontSize: 15}}>Hey, I'm</h5>,
-  <h1 >Samuel Abolade.</h1>,
-  <h6>Software Engineer</h6>
+  <h5 style={{ fontSize: 15 }}>Hey, I'm</h5>,
+  <h1>Samuel Abolade.</h1>,
+  <h6>Software Engineer</h6>,
 ];
 
-
-
 const Home = () => {
-
   return (
-
-    <StyledHeader id='home'>
+    <StyledHeader id="home">
       <section>
-        <motion.div initial="hidden" animate="visible" variants={containerVariant}>
-          {items.map((item, i) =>
-            <motion.div variants={childVariant} key={i}>{item}</motion.div>
-          )}
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={containerVariant}
+        >
+          {items.map((item, i) => (
+            <motion.div variants={childVariant} key={i}>
+              {item}
+            </motion.div>
+          ))}
         </motion.div>
         <CTA />
-      </section >
-    </ StyledHeader>
-
-  )
+      </section>
+    </StyledHeader>
+  );
 };
 
 export default Home;
