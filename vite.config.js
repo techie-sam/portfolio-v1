2de761e2773e92f82576becc524f84bfa9a697fd
TECHIE SAM <blessingabolade15@gmail.com>
2023-07-28 20:49:53 +0100

    used ssr

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    define: {
-        'process.env': process.env
-    }
+  plugins: [react()]
 })
-// server.hmr.overlay: false
